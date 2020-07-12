eec6596b72708699b9f1e0b3cba87b048f283b39
Jorge Cervantes <jorgecdev@gmail.com>
2020-07-12 00:41:25 -0500

    heroe data added

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,16 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService, Heroe } from 'src/app/services/heroes.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styles: [
+  ],
+  providers: [
+    HeroesService
   ]
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  heroes: Heroe[] = [];
+
+  constructor(
+    private _heroesService: HeroesService
+  ) { }
 
   ngOnInit(): void {
+    this.heroes = this._heroesService.getHeroes();
+    console.log(this.heroes);
   }
 
 }
